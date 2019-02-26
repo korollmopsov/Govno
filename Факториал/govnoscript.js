23107d8e803284fe048159723d2fda5073324099
Korollmopsov <glavnoenezobyt@gmail.com>
2019-02-26 10:08:42 +0700

    Теперь это говно жрет все что ты в него пихнешь

--- Факториал/govnoscript.js
+++ Факториал/govnoscript.js
@@ -1,13 +1,26 @@
- function fac(){
-	var c = document.getElementById('ch').value,C=c
-	if(c==1||c==0) {
-		C = 1
-	}
-	else
-	while(c!=2){
-		C=C*(c-1)
-		c=c-1
-	}
-	document.getElementById('rez').innerHTML = 'Забирай на: ' + C
+
+function truefactorial(num1) {
+	var rez = num1 ;
+	console.log(num1)
+	 if ( num1 === 1 || num1 === 0 ) {
+	 	rez = 1;
+	 	console.log(rez)
+	 } else if ( num1 < 0 ) {
+		 rez = 'Ты ебанутый бля так не бывает';
+	 } else if ( parseInt(num1) !== num1 ) {
+	 	rez = Math.sqrt ( 2 * Math.PI * num1 ) * Math.pow( num1 / Math.E , num1 ) * Math.pow ( Math.E , 1 / 12 * num1) ;
+	 }
+	   else {
+		 while (num1 !== 2) {
+			 rez = rez * (num1 - 1);
+			 num1 = num1 - 1;
+		 }
+	 }
+	return rez ;
 }
-	
+
+function factorial() {
+	var tnum1 = parseFloat(document.getElementById('num1').value) ;
+	var trez = truefactorial ( tnum1 ) ;
+	document.getElementById('trez').innerHTML = 'Забирай на: ' + trez ;
+}
