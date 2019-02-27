3ea0fd2b2b234a48cc9eae82eeb1f754b6be005a
Korollmopsov <glavnoenezobyt@gmail.com>
2019-02-28 00:32:35 +0700

    Лень делать чтоб несколько чисел было
    Но он пропускает дерьмо до мервой цифры

--- парс/parse.js
+++ парс/parse.js
@@ -1,24 +1,35 @@
 function filtr() {
     var tnum1 = document.getElementById('num1').value ;
-    trez = Tfiltr ( tnum1 ) ;
+    trez = parse( Tfiltr ( tnum1 ) ) ;
     document.getElementById('trez').innerHTML = 'Отфильтровано: ' + trez ;
 }
 
 function Tfiltr( A ) {
-    var i = 0 , B = '', j = 0 , y = 0 , c = 0 , p = 1 ;
-    for ( i ; i !== A.length ; i++ ) {
-        if ( ( +A[i] <= 9 && +A[i] >= 0 ) && A [i] != ' ' ) {
-            B += A[i] ;
+    var i = 0, B = '', y = 0 ;
+    for (i; i !== A.length; i++) {
+        if ((+A[i] <= 9 && +A[i] >= 0) && A [i] != ' ') {
+            B += A[i];
+            if (((( +A[ i + 1 ] <= 9 && +A[ i + 1 ] >= 0) && A [ i + 1 ] != ' ') || ( A[ i + 1 ] === '.' || A[ i + 1 ] === ',' ) ) === false )
+                break ;
         }
-        if ( (A[i] === '.' || A[i] === ',') && y === 0 ) {
-            B += A [i] ;
-            y = 1 ;
+        if ( A[i] === '.' || A[i] === ',' ) {
+            if ( y === 1 )
+                break ;
+            B += A [i];
+            y = 1;
         }
     }
-    y = 0 ;
-    i = 0 ;
-    if ( B [ j - 1 ] === '.' || B [ j - 1 ] === ',' )
-        B += '0' ;
+    y = 0;
+    i = 0;
+    if (B [ B.length ] === '.' || B [ B.length ] === ',')
+        B += '0';
+    if (B [0] === '.' || B [0] === ',')
+        B = '0' + B;
+    return B ;
+}
+
+function parse(B) {
+    var i = 0 , p = 1 , y = 0 , j = 0 , c = 0 ;
     while ( i <= B.length ) {
         if ( B [ i ] === '.' || B [ i ] === ',' ){
             p = 0 ;
